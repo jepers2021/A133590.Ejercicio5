75494ef9e29c13f1c25cb5dab5b92b8f5aad232d
Jorge <jepers2021@gmail.com>
2021-03-26 21:51:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A133590.Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.TreatControlCAsInput = true;
+            ConsoleKeyInfo cki;
+
+
+            Console.WriteLine("Ejercicio 5.");
+            Console.WriteLine("El programa lee las teclas presionadas hasta que se presiona CTRL+F");
+            while (true)
+            {
+                cki = Console.ReadKey();
+                if ((cki.Modifiers & ConsoleModifiers.Control) != 0 && cki.Key == ConsoleKey.F) break;
+            }
+
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
